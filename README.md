4ad2c0be7c0687e91198008502c14540d87931cd
Rainchi <35858349+rain931215@users.noreply.github.com>
2019-07-04 21:31:09 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# Discord-Verify-Bot
+# Discord Verify Bot
+A bot prevent spamming by using verify system
+
+# 指令
+!auth 產生序號
+!authkey [序號] 驗證序號
+
+# 安裝
+1. Github端
+    1. 登入現有帳號或註冊一個
+    2. 按此專案右上角的Fork
+  
+2. Heroku端
+    1. 前往Heroku官方網站並註冊一個新帳號,或使用現有帳號登入
+    2. 前往Heroku控制台[點我](https://dashboard.heroku.com) -> 右上角New -> 選擇Create new app
+    3. 切換到下個畫面後,填入App Name(自己取),完成後按Create app 
+    4. 接著會進入到App的Deploy子畫面 -> Deployment method選Github -> 按Connect to GitHub -> 跳出視窗點Authorize heroku
+    5. 連結後在repo-name那個打專案名後按Search -> 選搜尋結果裡剛剛Github Fork的專案按Connect
+    6. 接著先切換到Settings分頁 -> 按Reveal Config Vars,接著填入各項資訊,
+        格式 KEY:VALUE 不要複製貼上
+        BOT_TOKEN:請看下面的教學,可以順便邀機器人進群
+        VERIFIYED_ROLE_ID:請去Discord伺服器設定在要拿來當已驗證的身分組上面按右鍵然後複製ID
+        MESSAGE_ALREADY_VERIFYED:設定已驗證的玩家再次輸入指令時的提示訊息
+        MESSAGE_AUTH_KEY:設定當玩家輸入!auth時的訊息,以 <key> 代替產生的序號(執行時會自動轉成序號)
+        MESSAGE_AUTH_SUCCESS:設定當玩家驗證後產生的訊息
+        MESSAGE_AUTH_ERRORKEY:設定當玩家驗證時序號錯誤的訊息
+        MESSAGE_AUTH_NOTFOUNDKEY:設定當玩家驗證時找不到序號的訊息
+        MESSAGE_CMD_ERROR:設定當玩家驗證時未提供序號的訊息
+    7. 完成後切換回Deploy拉到下面按Manual deploy旁邊的Deploy
+    8. 切換到Resources分頁並重新整理頁面,會看到下面的Free Dynos有Web和Worker選項,按右邊的編輯把Web關掉、Worker打開
+    9. 完成,若還是不能用請來聯絡我,Discord:rainchi#0948
+
+# BOT_TOKEN獲取和邀請機器人進群
+1. 請參考 https://home.gamer.com.tw/creationDetail.php?sn=4303952 文章的第一步
+2. 在最後的畫面上點Token底下的Copy,就能得到Token去填入
+3. 填入完後回到最後的畫面切換到OAuth2到下面的SCOPES裡勾bot然後BOT PERMISSIONS勾Administrator
+4. 把SCOPES下面的連結複製並用瀏覽器打開,就能邀請了
+
---
+++ index.js
@@ -0,0 +1,53 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var authkey = new Map();
+
+client.on('ready', () => {
+    console.log("機器人已載入,請再次確認HEROKU平台上已設定下列變數:VERIFIYED_ROLE_ID、MESSAGE_ALREADY_VERIFYED、MESSAGE_AUTH_KEY、MESSAGE_AUTH_SUCCESS、MESSAGE_AUTH_ERRORKEY、MESSAGE_AUTH_NOTFOUNDKEY、MESSAGE_CMD_ERROR");
+})
+client.on('message', (receivedMessage) => {
+    if (receivedMessage.author == client.user) {
+        return;
+    }
+    if (receivedMessage.content.startsWith("!")) {
+        processCmd(receivedMessage);
+    }
+})
+function processCmd(receivedMessage) {
+    let fullCommand = receivedMessage.content.substr(1); // Remove the leading exclamation mark
+    let splitCommand = fullCommand.split(" "); // Split the message up in to pieces for each space
+    let primaryCommand = splitCommand[0]; // The first word directly after the exclamation is the command
+    let arguments = splitCommand.slice(1); // All other words are arguments/parameters/options for the command
+
+    if (primaryCommand == "auth") {
+        var val = Math.floor(1000 + Math.random() * 9000).toString();
+        if(receivedMessage.member.roles.has(process.env.VERIFIYED_ROLE_ID)){
+            receivedMessage.channel.send(process.env.MESSAGE_ALREADY_VERIFYED);
+            return;
+        }
+        authkey.set(receivedMessage.author.id, val);
+        receivedMessage.channel.send(process.env.MESSAGE_AUTH_KEY.replace(/\<key\>/g, val));
+    }else if (primaryCommand == "authkey") {
+        if(receivedMessage.member.roles.has(process.env.VERIFIYED_ROLE_ID)){
+            receivedMessage.channel.send(MESSAGE_ALREADY_VERIFYED);
+            return;
+        }
+        var val = authkey.get(receivedMessage.author.id);
+        if(arguments[0] != null && val!=null){
+            if(arguments[0] == val){
+                receivedMessage.member.addRole(VERIFIYED_ROLE_ID).catch(console.error);
+                receivedMessage.channel.send(process.env.MESSAGE_AUTH_SUCCESS);
+                authkey.delete(arguments[0]);
+            }else{
+                receivedMessage.channel.send(process.env.MESSAGE_AUTH_ERRORKEY);
+            }
+        }else if(arguments[0] == null){
+            receivedMessage.channel.send(process.env.MESSAGE_CMD_ERROR);
+        }else if(val == null){
+            receivedMessage.channel.send(process.env.MESSAGE_AUTH_NOTFOUNDKEY);
+        }
+    }
+}
+
+client.login(process.env.BOT_TOKEN)
